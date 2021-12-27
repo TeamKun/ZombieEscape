d19c2217d2809647e8ab0645b10e872c8ca731ef
dai-1103 <bosatsunumnum@gmail.com>
2021-12-27 14:03:20 +0900

    create project

---
+++ src/main/java/lab/kunmc/net/zombieescape/ZombieEscape.java
@@ -0,0 +1,17 @@
+package lab.kunmc.net.zombieescape;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZombieEscape extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
